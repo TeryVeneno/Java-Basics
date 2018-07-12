0c45473f0a27a705eabfd7fdfed371790998c39d
TeryVeneno <40150870+TeryVeneno@users.noreply.github.com>
2018-07-12 16:31:17 -0500

    f

---
+++ R_Arraylist.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class R_Arraylist {
+  public static void main(String[] args) {
+    int number;
+    Random ran = new Random(System.currentTimeMillis());
+    number = ran.nextInt(101);
+    System.out.println(number);
+  }
+}
